2d606392f8ae39cec227bfa2db2571bdea2a1017
Knut Tomasson Joramo <knut.joramo@gmail.com>
2022-03-08 20:34:07 +0100

    update file info

---
+++ script.py
@@ -0,0 +1,6 @@
+# file name:      script.py
+# creation date:  08.03.2022
+# author:         Knut Tomasson Joramo
+# 
+# portfolio project codecademy.com computersience 102
+# task is to build recommendation software
