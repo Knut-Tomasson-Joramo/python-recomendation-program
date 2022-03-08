9802aa4df849c1517e7dfdaa728a1d10b2d2c776
Knut Tomasson Joramo <knut.joramo@gmail.com>
2022-03-08 21:10:01 +0100

    error in word

--- resturants.py
+++ resturants.py
@@ -8,7 +8,7 @@ resturants = {
   "bing bong crocks": []
 }
 
-# took the easy way and downloaded resourse file from codecademy xD
+# took the easy way and downloaded resource file from codecademy xD
 types = ['german', 'japanese', 'vegetarian', 'french', 'african', 'american', 'barbecue', 'czech', 'chinese', 'thai',
          'mexican', 'indian', 'cafe', 'pizza', 'italian']
 
